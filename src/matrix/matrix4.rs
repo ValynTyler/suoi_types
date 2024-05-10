7b0516eea8043b2b0f57db526f9542b8e28a4731
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-05-10 11:43:01 +0300

    Added orthographica projection matrix

--- src/matrix/matrix4.rs
+++ src/matrix/matrix4.rs
@@ -170,4 +170,24 @@ impl Matrix4 {
             [0.0,        0.0, -1.0,                         0.0                                 ],
         )
     }
+
+    #[rustfmt::skip]
+    pub fn ortho(
+        left: f32,
+        right: f32,
+        bottom: f32,
+        top: f32,
+        near: f32,
+        far: f32,
+    ) -> Self {
+        let (r, l) = (right, left);
+        let (t, b) = (top, bottom);
+        let (f, n) = (far, near);
+        Self(
+            [2./(r-l),  0.,         0.,        -(r+l) / (r-l)   ],
+            [0.,        2./(t-b),   0.,        -(t+b) / (t-b)   ],
+            [0.,        0.,        -2./(f-n),  -(f+n) / (f-n)   ],
+            [0.,        0.,         0.,         1.              ],
+        )
+    }
 }
